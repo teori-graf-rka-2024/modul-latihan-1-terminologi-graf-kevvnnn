e0159b30b1d10b88b3f7bfde7038a2c5f890fa2f
Kevin Nicholas Nathanael <108829565+kevvnnn@users.noreply.github.com>
2025-03-16 23:02:17 +0700

    Update graph.py

---
+++ src/graph.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def create_graph(edges: list[tuple[int, int]]) -> nx.Graph:
+    return (nx.Graph(edges))
+
+def get_degree(G: nx.Graph, node: int) -> int:
+    return G.degree[node]
+
+def dfs_traversal(G: nx.Graph, start: int) -> list[int]:
+    return list(nx.dfs_preorder_nodes(G, start))
+
+def bfs_traversal(G: nx.Graph, start: int) -> list[int]:
+    return list(nx.bfs_tree(G, start))
+
+def find_shortest_path(G: nx.Graph, source: int, target: int) -> list[int]:
+    return list(nx.shortest_path(G, source, target))
+
+def visualize_graph(G: nx.Graph) -> None:
+    nx.draw(G)
+    plt.show()
